367f0d5b4cdebe228ba0805e6b357d01f7773ff8
Wagner Alves <wagneralvesdeoliveira92@outlook.com>
2019-11-01 19:39:18 -0300

    Commit inicial store server.

---
+++ store-server/src/test/java/com/acme/storeserver/StoreServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.storeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
